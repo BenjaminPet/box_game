da1879fa4792cd28c53a2e0b758bd89069b1358f
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2021-11-30 10:50:17 +0100

    2

--- box.js
+++ box.js
@@ -1,10 +1,22 @@
-var rtop = Math.floor(Math.random() * 100);
-var rwidth = Math.floor(Math.random() * 100);
 
-alert(rtop + " and " + rwidth)
+function random() {
+    var rtop = Math.floor(Math.random() * 100);
+    var rwidth = Math.floor(Math.random() * 100);
 
-function noe(t, w) {
-    document.getElementsByClassName("box").top = rtop
+    alert(rtop + " and " + rwidth)
+
+    document.getElementsByClassName("box").top = rtop;
+    document.getElementsByClassName("box").width = rwidth;
 }
 
-noe(rtop, rwidth);
+function time() {
+    for (i = 60; i != 0; i--) {
+        await delay(1000);
+        document.getElementsByClassName("time").innerhtml = "time " + i
+    }
+}
+
+function start() {
+    random();
+    time();
+}
