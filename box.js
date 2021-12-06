ac04f28ee5ea508a0b9f37f1f6afff690524834a
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2021-12-06 11:20:13 +0100

    noe

--- box.js
+++ box.js
@@ -19,11 +19,10 @@ function time() {
         counter--;
         // Display 'counter' wherever you want to display it.
         if (counter <= 0) {
+            scoreboard();
             window.location.href = "./scoreboard.html";
-            return;
         }else{
             document.getElementById("timer").innerHTML = "timer: " + counter;
-            scoreboard();
         }
     }, 1000);
 }
@@ -33,7 +32,6 @@ function start_round() {
     document.getElementById("box").style.display = "block";
     get_random();
     time();
-    console.log(noe);
 }
 function thelevel(x) {
     var boxd = document.querySelectorAll("box");
@@ -44,31 +42,18 @@ function thelevel(x) {
 }
 
 function new_game() {
-    var user = document.getElementById("username").value;
-    var levelf = document.getElementById("level").value;
-    console.log(levelf);
-    thelevel(levelf);
-    noe = [user, levelf];
+    var user = document.getElementById("username");
+    var levelf = document.getElementById("level");
+    thelevel(user.value, levelf.value);
+    noe = [user.value, levelf.value];
+    console.log(user.value + "    " + levelf.value + "     " + noe);
 
 }
 
 function scoreboard() {
 
-    
+}
 
-
-    var table = document.getElementById("TheScoreboard");
-
-    // Create an empty <tr> element and add it to the 1st position of the table:
-    var row = table.insertRow(0);
-
-    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-
-    // Add some text to the new cells:
-    cell1.innerHTML = "jfjf";
-    cell2.innerHTML = noe(1);
-    cell3.innerHTML = score;
+function checked() {
+    alert("hello")
 }
