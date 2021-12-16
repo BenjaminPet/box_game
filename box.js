3bac11b959c65f1e740f344e39eb0204fc765e61
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2021-12-16 14:23:09 +0100

    no

--- box.js
+++ box.js
@@ -18,14 +18,18 @@ function get_random() {
 
 function time() {
     var counter = 30;
-    var tick = new Audio("./images a")
+    var tick = new sound("./utstyr/448081__breviceps__tic-toc-click.wav").preload;
     document.getElementById("timer").innerHTML = "Timer: " + counter;
     var interval = setInterval(function () {
         counter--;
         // Display 'counter' wherever you want to display it.
         if (counter <= 0) {
             sessionStorage.setItem("score", score);
-            window.location.href = "./scoreboard.html";
+            document.querySelector(".tick").addEventListener("click", function() {
+
+                // ping clicked, play ping sound:
+                tick.play()
+            })
         } else {
             document.getElementById("timer").innerHTML = "Timer: " + counter;
             
